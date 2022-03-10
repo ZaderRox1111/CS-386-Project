e82de5f7e10874e6e0149fd487829820ed86d52f
Zachariah Derrick <zachariahderrick@gmail.com>
2022-03-09 17:14:02 -0700

    adding local run

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,8 +6,8 @@ const cors = require('cors');
 const { httpLogger } = require('./middlewares/logger')
 const router = require('./router/router');
 
-const port = 80;
-const ip = '104.168.247.236';
+let port = 80;
+let ip = '104.168.247.236';
 
 app.use(httpLogger());
 app.use(cors());
@@ -16,6 +16,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/server', router);
+
+// Check if it is local and change accordingly
+if (process.argv[2] === 'local') {
+    port = 8080;
+    ip = 'localhost'
+}
 
 app.listen(port, () => {
     logger.info(`Listening on port ${port}. Access here: http://${ip}:${port}/server`);
