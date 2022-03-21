9e6f1710d5163ebd85344bc3b68cfb19b78f81ce
conure512 <81276474+conure512@users.noreply.github.com>
2022-03-20 20:28:22 -0700

    Update Console.java

--- frontend/Console.java
+++ frontend/Console.java
@@ -1,16 +1,30 @@
 package thebestcsgroup.quikmafs;
+import java.io.InputStream;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+@SuppressWarnings("deprecation")
 public class Console {
 	public static void main(String[] args) {
 		new MatrixWindow().setVisible(true);
 	}
-	public static void sendToNodeServer(Object o) {
-		//TODO - change method return type, parameters, etc
-		//to appropriately send data to the server
+	public static String sendToNodeServer(Matrix mat) throws Exception {
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        HttpPost httpost = new HttpPost("http://104.168.247.236/server/matrix/calculate");
+        httpost.setEntity(new StringEntity(mat.toString()));
+        HttpResponse response = httpclient.execute(httpost);
+        httpclient.getConnectionManager().shutdown();
+        httpclient.close();
+        InputStream content=response.getEntity().getContent();
+        String result="";
+        char next;
+        do {
+        	next=(char)content.read();
+        	if(next!=-1)
+        		result+=next;
+        } while(next!=-1);
+        System.out.println("Received string: "+result);
+        return result;
 	}
-	public static Object getFromNodeServer() {
-		return null;
-		//TODO edit this method as well
-	}
-	//Also if you want to implement the server communications
-	//differently than having these two methods, let me know
 }
