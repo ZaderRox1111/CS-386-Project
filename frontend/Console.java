461ec5cbcbae5e48a5e053a756410263d97a9a61
conure512 <81276474+conure512@users.noreply.github.com>
2022-03-20 18:10:12 -0700

    Create Console.java

---
+++ frontend/Console.java
@@ -0,0 +1,16 @@
+package thebestcsgroup.quikmafs;
+public class Console {
+	public static void main(String[] args) {
+		new MatrixWindow().setVisible(true);
+	}
+	public static void sendToNodeServer(Object o) {
+		//TODO - change method return type, parameters, etc
+		//to appropriately send data to the server
+	}
+	public static Object getFromNodeServer() {
+		return null;
+		//TODO edit this method as well
+	}
+	//Also if you want to implement the server communications
+	//differently than having these two methods, let me know
+}
