2a5d3c4755d671c40ea4ec7e9b383fdd9be12a21
Zachariah Derrick <zachariahderrick@gmail.com>
2022-04-17 19:24:02 -0700

    fixing NaN issue

--- backend/src/handlers/probability.js
+++ backend/src/handlers/probability.js
@@ -3,7 +3,7 @@ const {P, C, union, intersect} = require('../util/probability');
 const calculatePnr = (req, res) => {
   const body = req.body;
 
-  const result = P(body.n, body.r);
+  const result = P(parseFloat(body.n), parseFloat(body.r));
 
   res.status(200).send(result + '');
 }
@@ -11,7 +11,7 @@ const calculatePnr = (req, res) => {
 const calculateCnr = (req, res) => {
   const body = req.body;
   
-  const result = C(body.n, body.r);
+  const result = C(parseFloat(body.n), parseFloat(body.r));
 
   res.status(200).send(result + '');
 }
@@ -19,7 +19,7 @@ const calculateCnr = (req, res) => {
 const calculateUnion = (req, res) => {
   const body = req.body;
   
-  const result = union(body.probA, body.probB);
+  const result = union(parseFloat(body.probA), parseFloat(body.probB));
 
   res.status(200).send(result + '');
 }
@@ -27,7 +27,7 @@ const calculateUnion = (req, res) => {
 const calculateIntersect = (req, res) => {
   const body = req.body;
   
-  const result = intersect(body.probA, body.probB);
+  const result = intersect(parseFloat(body.probA), parseFloat(body.probB));
 
   res.status(200).send(result + '');
 }
